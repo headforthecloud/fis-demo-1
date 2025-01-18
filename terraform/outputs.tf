02e2d2a7221e84d274f9b67b00af1652f68a769e
Simon Hanmer <simon.hanmer@gmail.com>
2025-01-18 16:47:44 +0000

    Add output with url for FIS experiment template.

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -7,3 +7,8 @@ output "lb_url" {
   description = "value of the load balancer URL"
   value       = "http://${aws_elb.this.dns_name}"
 }
+
+output "template_url" {
+  description = "Console access URL for the FIS template"
+  value       = "https://${var.region}.console.aws.amazon.com/fis/home?region=${var.region}#ExperimentTemplateDetails:ExperimentTemplateId=${aws_fis_experiment_template.this.id}"
+}
